5fc6fff86264063386bfe066005b90bf37531892
Lawrence Uricchio <uricchil@gmail.com>
2020-03-25 14:46:51 -0700

    Delete ABCsims.py

--- ABC_software/ABCsims.py
+++
@@ -1,5 +0,0 @@
-from MKinfer import inferParams
-import sys
-
-test = inferParams.inferTools(nsims=1000,Bfile='../../ABC/mk_with_positions_BGS.txt',testsim = False,pref='ABC',ABCsumsdir='ABC_sums_neg_new',datadir='../../ABC/output2')
-test.simulate()
